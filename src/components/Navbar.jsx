e72cf3940f64cffd50c04664902349b7f416996e
Christian <christiansmith2394@gmail.com>
2023-08-28 18:41:54 -0400

    working on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,35 @@
 import React from "react";
 import "../styles/navbar.css";
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
     <div className="navbar">
-      <h2>navbar here</h2>
+      <h1>navbar</h1>
+      {/* <nav className="nav">
+        <ul className="nav-items">
+          <li className="nav-item">
+            <NavLink to="/" exact activeClassName="active">
+              Home
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/about" exact activeClassName="active">
+              About
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/portfolios" exact activeClassName="active">
+              Projects
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/contact" exact activeClassName="active">
+              Contact
+            </NavLink>
+          </li>
+        </ul>
+      </nav> */}
     </div>
   );
 }
