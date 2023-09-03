248a2b32a6b4d0fef5ab25ad150f53df4efeb133
Christian <christiansmith2394@gmail.com>
2023-09-03 00:06:31 -0400

    routing working!

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,7 @@
 import React from "react";
-import Navbar from "./Navbar";
 
 function About() {
-  return (
-    <div>
-      <Navbar />
-      About
-    </div>
-  );
+  return <div>About</div>;
 }
 
 export default About;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import About from "./About.jsx";
 import Projects from "./Projects.jsx";
 import Contact from "./Contact.jsx";
 import Home from "./Home.jsx";
-import { NavLink } from "react-router-dom";
+import { NavLink, Route, Routes } from "react-router-dom";
 
 function Navbar() {
   return (
@@ -22,6 +22,12 @@ function Navbar() {
           <NavLink to="/contact">Contact</NavLink>
         </li>
       </ul>
+      <Routes>
+        <Route exact path="" element={<Home />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/contact" element={<Contact />} />
+        <Route exact path="/projects" element={<Projects />} />
+      </Routes>
     </div>
   );
 }
