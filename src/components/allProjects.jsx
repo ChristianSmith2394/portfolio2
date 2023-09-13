4319c1dfa2d8185cb0ba9697cb40b3d103fa41ea
Christian <christiansmith2394@gmail.com>
2023-09-13 19:06:29 -0400

    adding in projects page and projects

---
+++ src/components/allProjects.jsx
@@ -0,0 +1,90 @@
+import port1 from "../img/portImages/AmazonClone.png";
+import port2 from "../img/portImages/NetflixClone.png";
+import port3 from "../img/portImages/Calculator.png";
+import port4 from "../img/portImages/Todo.png";
+import port5 from "../img/portImages/Flight.png";
+import port6 from "../img/portImages/Hangman.png";
+import port7 from "../img/portImages/Dashboard.png";
+
+const projects = [
+  {
+    id: 1,
+    category: "React Js",
+    link1: "",
+    link2: "https://github.com/ChristianSmith2394/dashboard",
+    icon1: "Website",
+    icon2: "GitHub",
+    image: port7,
+    title: "Admin Dashboard",
+    description:
+      "A place to see all sorts of activity like sales and orders for a company.",
+  },
+  {
+    id: 2,
+    category: "React Js",
+    link1: "",
+    link2: "https://github.com/CKT-Technologies/ckt-flights",
+    icon1: "Website",
+    icon2: "GitHub",
+    image: port5,
+    title: "Flight Scheduler",
+    description: "A way to search for flights and prices anywhere in the world",
+  },
+  {
+    id: 3,
+    category: "React Js",
+    link1: "",
+    link2: "https://github.com/ChristianSmith2394/to-do",
+    icon1: "Website",
+    icon2: "GitHub",
+    image: port4,
+    title: "Todo App",
+    description: "A simple todo application",
+  },
+  {
+    id: 4,
+    category: "React Js",
+    link1: "https://ecom-store-cs94.web.app/",
+    link2: "https://github.com/ChristianSmith2394/ecom-store",
+    icon1: "Website",
+    icon2: "GitHub",
+    image: port1,
+    title: "Amazon Clone",
+    description: "A fully functioning Amazon reproduction",
+  },
+  {
+    id: 5,
+    category: "React Js",
+    link1: "https://netflix-clone-2374a.web.app/",
+    link2: "https://github.com/ChristianSmith2394/netflix-clone",
+    icon1: "Website",
+    icon2: "GitHub",
+    image: port2,
+    title: "Netflix Clone",
+    description: `A representation of Netflix's homepage`,
+  },
+  {
+    id: 6,
+    category: "React Js",
+    link1: "",
+    link2: "https://github.com/ChristianSmith2394/calculator",
+    icon1: "Website",
+    icon2: "GitHub",
+    image: port3,
+    title: "Calculator",
+    description: "A simple stylish calculator",
+  },
+  {
+    id: 7,
+    category: "TypeScript",
+    link1: "https://hangman-tsx.web.app/",
+    link2: "https://github.com/ChristianSmith2394/hangman",
+    icon1: "Website",
+    icon2: "GitHub",
+    image: port6,
+    title: "Hangman Game",
+    description: "A simple way to play Hangman online",
+  },
+];
+
+export default projects;
