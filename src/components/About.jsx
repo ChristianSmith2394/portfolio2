09d8a45c737c4c14ec1ff48031290b868a53889f
Christian <christiansmith2394@gmail.com>
2024-02-26 21:10:51 -0500

    updated font weight on navbar and biography

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,15 @@ function About() {
         <h2>Photo goes here - import one</h2>
       </div>
       <div className="rightSide">
-        <h2>Biography goes here - downloadable resume underneath</h2>
+        <h2>Add downloadable resume underneath</h2>
+        <p>
+          I’m a Fullstack Developer who is passionate about the JavaScript
+          ecosystem. I am most skilled and passionate about Node (Express) and
+          React. Growing up I've always been interested in technology and
+          creating/building things. My passion began with building things out of
+          Lego's, K'Nex, etc. My dream career position is Fullstack Development
+          that focuses on Javascript, Node, React and SQLite3.
+        </p>
       </div>
     </div>
   );
