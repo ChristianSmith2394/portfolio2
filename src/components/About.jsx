d93827dda4129c987b7be3e6493aa5cafdeccc08
Christian <christiansmith2394@gmail.com>
2024-02-26 20:46:27 -0500

    setting up about me page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import "../styles/about.css";
 
 function About() {
-  return <div></div>;
+  return (
+    <div className="aboutContainer">
+      <div className="leftSide">
+        <h2>Part 1</h2>
+      </div>
+      <div className="rightSide">
+        <h2>Part 2</h2>
+      </div>
+    </div>
+  );
 }
 
 export default About;
