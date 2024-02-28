a6e5b7abfdf3eef926f8fda1a863334f328d01e2
Christian <christiansmith2394@gmail.com>
2024-02-28 15:18:05 -0500

    edited bio

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,11 +15,11 @@ function About() {
         <h2>Add downloadable resume underneath</h2>
         <p>
           I’m a Fullstack Developer who is passionate about the JavaScript
-          ecosystem. I am most skilled and passionate about Node (Express) and
-          React. Growing up I've always been interested in technology and
+          ecosystem. I am most skilled and interested about Node (Express) and
+          React. Growing up I've always been intrigued in technology and
           creating/building things. My passion began with building things out of
-          Lego's, K'Nex, etc. My dream career position is Fullstack Development
-          that focuses on Javascript, Node, React and SQLite3.
+          Lego's, K'Nex, etc. My dream position is Fullstack Development that
+          focuses on Javascript, Node, React and SQLite3.
         </p>
       </div>
     </div>
